ecbb085354cc515aaeddf38a3781e1324dba1c3e
Yerlan110110 <eardabek@gmail.com>
2025-10-04 22:37:17 +0500

    Add func hello

--- TelegramBot/Тelegram_Bot.py
+++ TelegramBot/Тelegram_Bot.py
@@ -3,9 +3,16 @@ import telebot
 bot = telebot.TeleBot('8475840146:AAFV6jH14M1Bma-gPT0PoXpULRw-KF8AgKc')
 
 
+@bot.message_handler()
+def hello(message):
+    if message.text.lower() == 'привет':
+        start(message)
+
+
+
 @bot.message_handler(commands =['start'])
 def start(message):
-    bot.send_message(message.chat.id,f'<b>Привет {message.from_user.first_name}</b>',parse_mode='html')
+    bot.send_message(message.chat.id,f'<b>Привет {message.from_user.first_name}!</b>',parse_mode='html')
 
 
 @bot.message_handler(commands =['help'])
