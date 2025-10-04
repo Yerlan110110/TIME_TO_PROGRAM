91c86217418dff7456d63563b103d6c8c9bf8a23
Yerlan110110 <eardabek@gmail.com>
2025-10-04 22:25:52 +0500

    Add func start,help

---
+++ TelegramBot/Тelegram_Bot.py
@@ -0,0 +1,27 @@
+import telebot
+
+bot = telebot.TeleBot('8475840146:AAFV6jH14M1Bma-gPT0PoXpULRw-KF8AgKc')
+
+
+@bot.message_handler(commands =['start'])
+def start(message):
+    bot.send_message(message.chat.id,f'<b>Привет {message.from_user.first_name}</b>',parse_mode='html')
+
+
+@bot.message_handler(commands =['help'])
+def hp(message):
+    bot.send_message(message.chat.id,'<b><u>/start</u> - начать</b>',parse_mode='html')
+
+
+bot.infinity_polling()
+
+
+
+
+
+
+
+
+
+
+
