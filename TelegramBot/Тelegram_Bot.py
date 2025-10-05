d093f3a66bd570e06527dfbb705ae2df03f56b43
Yerlan110110 <eardabek@gmail.com>
2025-10-05 20:13:58 +0500

    Add func get_photo,improved the function site

--- TelegramBot/Тelegram_Bot.py
+++ TelegramBot/Тelegram_Bot.py
@@ -1,15 +1,11 @@
 import telebot
-
+from telebot import types
 bot = telebot.TeleBot('8475840146:AAFV6jH14M1Bma-gPT0PoXpULRw-KF8AgKc')
 
 
-@bot.message_handler()
-def hello(message):
-    if message.text.lower() == 'привет':
-        start(message)
-    elif message.text.lower() == 'id':
-        bot.reply_to(message,message.from_user.id)
-
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    bot.reply_to(message, 'good photo')
 
 
 @bot.message_handler(commands =['start'])
@@ -20,6 +16,21 @@ def start(message):
 @bot.message_handler(commands =['help'])
 def hp(message):
     bot.send_message(message.chat.id,'<b><u>/start</u> - начать</b>',parse_mode='html')
+
+
+@bot.message_handler(commands =['site','website'])
+def site(message):
+    markup = types.InlineKeyboardMarkup()
+    url = 'https://photofile.ru/wp-content/uploads/2024/04/79b9dpy0sag.jpg'
+    markup.add(types.InlineKeyboardButton('Открыть сайт',url = url))
+    bot.send_message(message.chat.id, 'Вот сайт👇',reply_markup=markup)
+
+bot.message_handler()
+def hello(message):
+    if message.text.lower() == 'привет':
+        start(message)
+    elif message.text.lower() == 'id':
+        bot.reply_to(message,message.from_user.id)
 
 
 bot.infinity_polling()
